8233aa91581aa1848b5ef08386b81b9cd37a398c
Jan Strnad <strnad23@student.vspj.cz>
2025-09-29 00:18:47 +0200

    pipe phpactor diagnostics on save into telescope

--- init.lua
+++ init.lua
@@ -413,6 +413,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
@@ -995,6 +996,22 @@ require('lazy').setup({
       lazy = '💤 ',
     },
   },
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = '*.php',
+  callback = function()
+    local diagnostics = vim.diagnostic.get(0)
+    if #diagnostics > 0 then
+      -- Small delay to let file save complete
+      vim.defer_fn(function()
+        require('telescope.builtin').diagnostics {
+          bufnr = 0,
+          severity_limit = vim.diagnostic.severity.WARN, -- Only show warnings and errors
+        }
+      end, 100)
+    end
+  end,
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
