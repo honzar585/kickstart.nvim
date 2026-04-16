f48b4eb4dd4c2a567e191db6758435cd3762bc50
Jan Strnad <strnad23@student.vspj.cz>
2026-04-16 10:41:37 +0200

    added neogit

--- init.lua
+++ init.lua
@@ -654,7 +654,7 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         -- gopls = {},
-        basedpyright = {},
+        ty = {},
         phpactor = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
